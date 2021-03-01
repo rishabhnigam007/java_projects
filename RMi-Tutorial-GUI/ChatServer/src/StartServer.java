2099bef26a60219f973908e83233d83b25318bbe
Rishabh Nigam <65018070+rishabhnigam007@users.noreply.github.com>
2021-03-01 22:42:25 +0530

    Add files via upload
    Chat Application using Entranet

---
+++ RMi-Tutorial-GUI/ChatServer/src/StartServer.java
@@ -0,0 +1,27 @@
+import java.rmi.*;
+
+import java.rmi.server.*;
+
+ 
+public class StartServer 
+{
+
+	public static void main(String[] args)
+	{
+
+	try {
+		
+//System.setSecurityManager(new RMISecurityManager());
+
+	java.rmi.registry.LocateRegistry.createRegistry(1099);
+	                 ChatServerInt b=new ChatServer();	
+			Naming.rebind("rmi://192.168.1.2/myabc", b);
+		
+	System.out.println("[System] Chat Server is ready.");
+		
+}
+catch (Exception e) {
+					System.out.println("Chat Server failed: " + e);
+			}
+	}
+}
